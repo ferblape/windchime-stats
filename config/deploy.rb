bcb67eb7d0126a3bf7605a9f711f9344a07314c2
Fernando Blat <ferblape@gmail.com>
2011-12-26 15:16:35 -0300

    Restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,9 +41,7 @@ namespace :deploy do
     run "#{current_path}/bin/windchime-stats-realtime.sh stop"
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{current_path}/bin/windchime-stats.sh stop"
-    run "#{current_path}/bin/windchime-stats-realtime.sh stop"
-    run "#{current_path}/bin/windchime-stats.sh start"
-    run "#{current_path}/bin/windchime-stats-realtime.sh start"
+    run "#{current_path}/bin/windchime-stats.sh restart"
+    run "#{current_path}/bin/windchime-stats-realtime.sh restart"
   end
 end
